972cf9a9576d46037309f309cd4c888e9717b600
felice99111 <felix.kreitmeier@gmail.com>
2018-08-05 13:55:54 +0200

    Header für ListView erstellt, Goal Items können jetzt Tag & Monat zurückgeben

---
+++ dblApp/app/src/main/java/com/example/felix/dailybucketlist/Adapter/BucketListAdapter.java
@@ -0,0 +1,28 @@
+package com.example.felix.dailybucketlist.Adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.example.felix.dailybucketlist.Goals.Goal;
+
+import java.util.List;
+
+public class BucketListAdapter extends ArrayAdapter<Goal>{
+
+    //Nimm dir ÜB07 TableItemAdapter als "Vorbild". Die @NonNull Querys kannst du so stehen lassen, die kommen von der Datenbank
+
+
+    public BucketListAdapter(@NonNull Context context, int resource, @NonNull List<Goal> objects) {
+        super(context, resource, objects);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        return super.getView(position, convertView, parent);
+    }
+}
--- dblApp/app/src/main/java/com/example/felix/dailybucketlist/MainActivity.java
+++ dblApp/app/src/main/java/com/example/felix/dailybucketlist/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.felix.dailybucketlist;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -19,9 +22,7 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText inputName, inputReached;
-    private Button createButton, clearAllButton, clearFirstButton, updateFirstButton;
-    private List<Goal> goals;
+    private List<Goal> goalDB;
     private ListView listView;
     private ArrayAdapter<Goal> adapter;
 
@@ -30,83 +31,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        goals = BucketListDatabase.getInstance(this).readAllGoals();
-
+        goalDB = BucketListDatabase.getInstance(this).readAllGoals();
+        goalDB.clear(); //aktuell sind in der DB zwei provisoriche Goals zu Testzwecken. Damti diese in der App nicht angezeigt werden, wird die DB beim Start
+                        //"aufgeräumt". Falls du mit der DB arbeiten möchtest, nimm die Methode raus :)
         initUI();
     }
 
     private void initUI() {
-        inputName = (EditText) findViewById(R.id.goal);
-        inputReached = (EditText) findViewById(R.id.reached);
-        createButton = (Button) findViewById(R.id.createButton);
-        clearAllButton = (Button) findViewById(R.id.clearAllButton);
-        clearFirstButton = (Button) findViewById(R.id.clearFirstButton);
-        updateFirstButton = (Button) findViewById(R.id.updateFirstButton);
         initListView();
-
-        createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BucketListDatabase database = BucketListDatabase.getInstance(MainActivity.this); //Kontext des onClickListeners, deshalb würde nur this eine falsche Referenz übergeben
-                database.createGoal(new Goal("Zimmer streichen", Calendar.getInstance()));
-                database.createGoal(new Goal("Joggen"));
-
-                refreshListView();
-            }
-        });
-
-        clearAllButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BucketListDatabase database = BucketListDatabase.getInstance(MainActivity.this);
-                database.deleteAllGoals();
-                refreshListView();
-            }
-        });
-
-        clearFirstButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                BucketListDatabase database = BucketListDatabase.getInstance(MainActivity.this);
-
-                if(goals.size() > 0 ) {
-                    database.deleteGoal(goals.get(0));
-                }
-                refreshListView();
-            }
-        });
-
-        updateFirstButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(goals.size() > 0) {
-                    BucketListDatabase database = BucketListDatabase.getInstance(MainActivity.this);
-                    Random r = new Random();
-                    goals.get(0).setName(String.valueOf(r.nextInt()));
-                    database.updateGoal(goals.get(0));
-                    refreshListView();
-                }
-            }
-        });
     }
 
     private void initListView() {
         listView = (ListView) findViewById(R.id.bucketListView);
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, goals);
+        View header = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.table_item, null);
+        listView.addHeaderView(header);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, goalDB);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Object element = parent.getAdapter().getItem(position);
-            }
-        });
     }
 
     private void refreshListView() {
-        goals.clear();
-        goals.addAll(BucketListDatabase.getInstance(this).readAllGoals());
+        goalDB.clear();
+        goalDB.addAll(BucketListDatabase.getInstance(this).readAllGoals());
         adapter.notifyDataSetChanged();
     }
 
 
 }
+
+/** Methode, um neuen Eintrag in die Datenbank zu erstellen
+ BucketListDatabase database = BucketListDatabase.getInstance(MainActivity.this); //Kontext des onClickListeners, deshalb würde nur this eine falsche Referenz übergeben
+ database.createGoal(new Goal("Zimmer streichen", Calendar.getInstance()));
+ database.createGoal(new Goal("Joggen"));
+
+ refreshListView(); **/
+
+/** Methode, um alle Einträge zu löschen
+ BucketListDatabase database = BucketListDatabase.getInstance(MainActivity.this);
+ database.deleteAllGoals();
+ refreshListView(); **/
+
+/** Methode, um obersten Eintrag der Database zu löschen
+ BucketListDatabase database = BucketListDatabase.getInstance(MainActivity.this);
+
+ if(goals.size() > 0 ) {
+ database.deleteGoal(goals.get(0));
+ }
+ refreshListView(); **/
+
+/** Methode, um ersten Datenbankeintrag zu upgraden (mit random nummer zu versehen): updatet wirklich nur das erste?
+ if(goals.size() > 0) {
+ BucketListDatabase database = BucketListDatabase.getInstance(MainActivity.this);
+ Random r = new Random();
+ goals.get(0).setName(String.valueOf(r.nextInt()));
+ database.updateGoal(goals.get(0));
+ refreshListView(); **/
