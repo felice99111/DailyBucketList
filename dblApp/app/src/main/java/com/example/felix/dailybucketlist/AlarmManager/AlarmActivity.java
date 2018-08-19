5089ab837139ca781439ce288eb987a1153a5466
felice99111 <felix.kreitmeier@gmail.com>
2018-08-19 12:49:01 +0200

    Notifications werden zu ausgewählter Uhrzeit gesendet, zugehöriger Intent wird noch nicht ausgelöst

--- dblApp/app/src/main/java/com/example/felix/dailybucketlist/AlarmManager/AlarmActivity.java
+++ dblApp/app/src/main/java/com/example/felix/dailybucketlist/AlarmManager/AlarmActivity.java
@@ -1,17 +1,22 @@
 package com.example.felix.dailybucketlist.AlarmManager;
 
 import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TimePicker;
 import android.widget.Toast;
 
+import com.example.felix.dailybucketlist.Config;
 import com.example.felix.dailybucketlist.R;
 
 import java.util.Calendar;
@@ -25,11 +30,12 @@ public class AlarmActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
-
+        createNotificationChannel();
         timePicker = (TimePicker) findViewById(R.id.timePicker);
 
         buttonSetAlarm = (Button) findViewById(R.id.buttonSetAlarm);
         buttonSetAlarm.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Calendar calendar = Calendar.getInstance();
@@ -55,6 +61,7 @@ public class AlarmActivity extends AppCompatActivity {
                 }
 
                 setAlarm(calendar.getTimeInMillis());
+
             }
         });
 
@@ -65,8 +72,27 @@ public class AlarmActivity extends AppCompatActivity {
         Intent intent = new Intent(this, AlarmNotificationReceiver.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
         //Setting alarmManager to repeat every day
-        alarmManager.setRepeating(AlarmManager.RTC, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, timeInMillis, AlarmManager.INTERVAL_DAY, pendingIntent);
         Toast.makeText(this, "Alarm is set", Toast.LENGTH_SHORT).show();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+
+        //Strings etc. auslagern und korrekt setzen!
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Channel name";
+            String description = "channel description";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(Config.CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
 }
--- dblApp/app/src/main/java/com/example/felix/dailybucketlist/AlarmManager/AlarmNotificationReceiver.java
+++ dblApp/app/src/main/java/com/example/felix/dailybucketlist/AlarmManager/AlarmNotificationReceiver.java
@@ -2,19 +2,26 @@ package com.example.felix.dailybucketlist.AlarmManager;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 import com.example.felix.dailybucketlist.Config;
+import com.example.felix.dailybucketlist.MainActivity;
 import com.example.felix.dailybucketlist.R;
 
 public class AlarmNotificationReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        //click auf not führt nicht in die main activity!
+        /*intent = new Intent(context, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);*/
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, Config.CHANNEL_ID);
 
         builder.setAutoCancel(true)
                 .setDefaults(Notification.DEFAULT_ALL)
@@ -22,10 +29,13 @@ public class AlarmNotificationReceiver extends BroadcastReceiver {
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle("CONTENT TITLE")
                 .setContentText("THIS IS MY ALARM")
+                .setPriority(Notification.PRIORITY_DEFAULT)
                 .setDefaults(Notification.DEFAULT_LIGHTS | Notification.DEFAULT_SOUND)
+                /*.setContentIntent(pendingIntent)*/
                 .setContentInfo("Info");
 
         NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, builder.build());
+
     }
 }
