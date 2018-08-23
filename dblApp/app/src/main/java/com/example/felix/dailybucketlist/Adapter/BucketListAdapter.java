03abb2a8e4ea51da5db82e6c6a89dc273ab75587
johannaverena <hanni.hanna@web.de>
2018-08-23 21:57:08 +0200

    Arbeit am Adapter - noch nicht fertig.

--- dblApp/app/src/main/java/com/example/felix/dailybucketlist/Adapter/BucketListAdapter.java
+++ dblApp/app/src/main/java/com/example/felix/dailybucketlist/Adapter/BucketListAdapter.java
@@ -9,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+
 import com.example.felix.dailybucketlist.Goals.Goal;
 
 import java.util.List;
@@ -17,21 +18,45 @@ public class BucketListAdapter extends ArrayAdapter<Goal>{
 
     //Nimm dir ÜB07 TableItemAdapter als "Vorbild". Die @NonNull Querys kannst du so stehen lassen, die kommen von der Datenbank
 
-    private List<Goal> table;
+    private List<Goal> goals;
     private Context context;
     private int resource;
+
 
     public BucketListAdapter(@NonNull Context context, int resource, @NonNull List<Goal> objects) {
         super(context, resource, objects);
         this.context = context;
         this.resource = resource;
-        table = objects;
+        goals = objects;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-        return super.getView(position, convertView, parent);
+       // return super.getView(position, convertView, parent);
+
+        View v = convertView;
+
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(resource, null);
+        }
+/*
+        TextView monday = (TextView) v.findViewById(android.R.id.monday);
+        TextView tuesday = (TextView) v.findViewById(android.R.id.tuesday);
+        TextView wednesday = (TextView) v.findViewById(android.R.id.wednesday);
+        TextView thursday = (TextView) v.findViewById(android.R.id.thursday);
+        TextView friday = (TextView) v.findViewById(android.R.id.friday);
+
+        Goal item = goals.get(position);
+        v.setBackgroundResource(getColorForRank(item.getRank()));
+        rank.setText(String.valueOf(item.getRank()));
+        team.setText(item.getTeam());
+        playedGames.setText(String.valueOf(item.getPlayedGames()));
+        points.setText(String.valueOf(item.getPoints()));
+        goalDifference.setText(item.getGoals() + ":" + item.getGoalsAgainst()); */
+
+        return v;
 
     }
 }
