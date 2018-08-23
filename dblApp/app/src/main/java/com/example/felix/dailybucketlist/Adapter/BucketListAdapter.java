fb93e51511c9ae8a8cbf431884b7207f7c3937c0
johannaverena <hanni.hanna@web.de>
2018-08-23 22:05:16 +0200

    Arbeit am Adapter - passt noch nicht.

--- dblApp/app/src/main/java/com/example/felix/dailybucketlist/Adapter/BucketListAdapter.java
+++ dblApp/app/src/main/java/com/example/felix/dailybucketlist/Adapter/BucketListAdapter.java
@@ -3,9 +3,11 @@ package com.example.felix.dailybucketlist.Adapter;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import com.example.felix.dailybucketlist.Database.BucketListDatabase;
 import com.example.felix.dailybucketlist.Goals.Goal;
@@ -17,10 +19,7 @@ public class BucketListAdapter extends ArrayAdapter<Goal>{
 
     //über die List (sollte sich wie eine normale ArrayList verhalten) erhältst du Zugriff auf alle Goals in der Datenbank
     List <Goal> goalDB;
-
-    // Test
-    private void test() {
-    }
+    private Context context;
 
     //Nimm dir ÜB07 TableItemAdapter als "Vorbild". Die @NonNull Querys kannst du so stehen lassen, die kommen von der Datenbank
 
@@ -28,11 +27,26 @@ public class BucketListAdapter extends ArrayAdapter<Goal>{
     public BucketListAdapter(@NonNull Context context, int resource, @NonNull List<Goal> objects) {
         super(context, resource, objects);
         goalDB = BucketListDatabase.getInstance(context).readAllGoals();
+        this.context = context;
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         return super.getView(position, convertView, parent);
+
+/*
+        if (v == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = inflater.inflate(android.R.layout.table_item, null);
+        }
+
+        TextView monday = (TextView) v.findViewById(android.R.id.monday);
+        TextView tuesday = (TextView) v.findViewById(android.R.id.tuesday);
+        TextView wednesday = (TextView) v.findViewById(android.R.id.wednesday);
+        TextView thursday = (TextView) v.findViewById(android.R.id.thursday);
+        TextView friday = (TextView) v.findViewById(android.R.id.friday);
+
+        return v; */
     }
 }
